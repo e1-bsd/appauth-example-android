c18651008b349fe976244e5838b5bdbc91610206
Rubén Illodo Brea <me@rubenillodo.com>
2017-11-28 15:09:56 +0800

    Working

---
+++ app/src/main/java/com/ef/appauthexample/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.ef.appauthexample
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    companion object {
+        val LOG_TAG = "AppAuthSample"
+    }
+
+}
